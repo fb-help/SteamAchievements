593998a2286854e93bfc4193d80a7bb081296e0a
Bladerama <patrick_wauschek@mailbox.org>
2020-07-28 13:12:52 -0500

    Apache Commons IO for file and dir error checking
    Checks csv file overwrite and debug flag for true and false input.

--- src/main/java/com/axlsreborn/ProjectProperties.java
+++ src/main/java/com/axlsreborn/ProjectProperties.java
@@ -1,25 +1,25 @@
 package com.axlsreborn;
 
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 public class ProjectProperties {
-    public static final String KEY_STEAM_WEB_API_KEY  = "steam.web_api_key";
-    public static final String KEY_STEAM_APP_ID       = "steam.app_id";
-    public static final String KEY_CSV_DELIMITER      = "csv.delimiter";
-    public static final String KEY_CSV_FILE_PATH      = "csv.file_path";
+    public static final String KEY_STEAM_WEB_API_KEY = "steam.web_api_key";
+    public static final String KEY_STEAM_APP_ID = "steam.app_id";
+    public static final String KEY_CSV_DELIMITER = "csv.delimiter";
+    public static final String KEY_CSV_FILE_PATH = "csv.file_path";
     public static final String KEY_CSV_FILE_OVERWRITE = "csv.file_overwrite";
-    public static final String KEY_DEBUG_FLAG         = "debug.flag";
+    public static final String KEY_DEBUG_FLAG = "debug.flag";
 
-    private final String  apiKey;
-    private final int     appId;
-    private final char    csvDelimiter;
-    private final String  csvFilePath;
+    private final String apiKey;
+    private final int appId;
+    private final char csvDelimiter;
+    private final String csvFilePath;
     private final boolean csvFileOverwriteFlag;
     private final boolean csvFileErrorFlag;
     private final boolean debugFlag;
@@ -28,10 +28,8 @@ public class ProjectProperties {
         String apiKey;
         int appId;
         char csvDelimiter = ' ';
-        String csvFilePathStr;
-        Path csvFilePath;
+        String csvFilePath;
         boolean csvFileErrorFlag = false;
-        String csvFileOverwrite;
         boolean csvFileOverwriteFlag = false;
         boolean debugFlag = false;
 
@@ -54,70 +52,102 @@ public class ProjectProperties {
             String steamAppIdStr = properties.getProperty(KEY_STEAM_APP_ID);
             appId = Integer.parseInt(steamAppIdStr);
         } catch (NumberFormatException e) {
+            System.err.printf("App ID: [%s]\n", KEY_STEAM_APP_ID);
             throw new ProjectPropertiesException(e.getLocalizedMessage());
         }
 
         apiKey = properties.getProperty(KEY_STEAM_WEB_API_KEY);
         String csvDelimiterStr = properties.getProperty(KEY_CSV_DELIMITER);
-        csvFilePathStr = properties.getProperty(KEY_CSV_FILE_PATH);
-        csvFilePath = Paths.get(csvFilePathStr);
-        csvFileOverwrite = properties.getProperty(KEY_CSV_FILE_OVERWRITE);
+        csvFilePath = properties.getProperty(KEY_CSV_FILE_PATH);
+        String csvFileOverwriteStr = properties.getProperty(KEY_CSV_FILE_OVERWRITE);
         String debugFlagStr = properties.getProperty(KEY_DEBUG_FLAG);
-
-        if (csvFileOverwrite != null && csvFileOverwrite.toUpperCase().equals("TRUE")) {
-            csvFileOverwriteFlag = true;
-        }
-
-        try {
-            if (Files.exists(csvFilePath) && !csvFileOverwriteFlag) {
-                csvFileErrorFlag = true;
-                System.err.println(
-                        "Please enable CSV file overwriting or specify another CSV file path in properties file.");
-                System.out.println("csvFileOverwriteFlag = [" + csvFileOverwriteFlag + "]");
-                System.err.println("csvFilePath = [" + csvFilePath + "]");
-            }
-            else if (!Files.exists(csvFilePath)) {
-                System.err.println("File path is incorrect in properties file.");
-                System.err.println("csvFilePath = [" + csvFilePath + "]");
-                csvFileErrorFlag = true;
-            }
-        } catch (SecurityException e) {
-            throw new ProjectPropertiesException(e.getLocalizedMessage());
-        }
-
-        if (debugFlagStr != null && debugFlagStr.toUpperCase().equals("TRUE")) {
-            debugFlag = true;
-        }
-
-        if (debugFlag) {
-            System.out.println("Main.main(): apiKey = [" + apiKey + "]");
-            System.out.println("Main.main(): appId = [" + appId + "]");
-            System.out.println("Main.main(): csvDelimiter = [" + csvDelimiterStr + "]");
-            System.out.println("Main.main(): csvFilePathStr = [" + csvFilePathStr + "]");
-            System.out.println("Main.main(): csvFileOverwriteFlag = [" + csvFileOverwriteFlag + "]");
-            System.out.println("Main.main(): csvFileErrorFlag = [" + csvFileErrorFlag + "]");
-            System.out.println("Main.main(): debugFlag = [" + debugFlag + "]");
-        }
 
         if (apiKey == null || apiKey.isEmpty()) {
             System.err.printf("Expected key: [%s]\n", KEY_STEAM_WEB_API_KEY);
             throw new ProjectPropertiesException("API Key is not defined in properties file");
         }
+
         if (csvDelimiterStr == null || csvDelimiterStr.length() != 1) {
             System.err.printf("csvDelimiter = [%s]\n", KEY_CSV_DELIMITER);
             throw new ProjectPropertiesException("CSV delimiter is not defined in properties file");
         } else {
             csvDelimiter = csvDelimiterStr.charAt(0);
         }
-        if (csvFilePathStr == null || csvFilePathStr.isEmpty()) {
-            System.err.printf("csvFilePathStr = [%s]\n", KEY_CSV_FILE_PATH);
+
+        if (csvFilePath == null || csvFilePath.isEmpty()) {
+            System.err.printf("csvFilePath = [%s]\n", KEY_CSV_FILE_PATH);
             throw new ProjectPropertiesException("CSV file path is not defined in properties file");
+        }
+
+        if (debugFlagStr == null || debugFlagStr.isEmpty()) {
+            System.err.printf("debugFlagStr = [%s]\n", KEY_DEBUG_FLAG);
+            throw new ProjectPropertiesException("Debug flag is not defined in properties file");
+        } else if (debugFlagStr.toUpperCase().equals("TRUE")) {
+            debugFlag = true;
+        } else if (debugFlagStr.toUpperCase().equals("FALSE")) {
+            debugFlag = false;
+        } else {
+            throw new ProjectPropertiesException("Set debug flag to true or false");
+        }
+
+        if (csvFileOverwriteStr == null || csvFileOverwriteStr.isEmpty()) {
+            System.out.printf("csvFileOverwriteStr = [%s]\n", KEY_CSV_FILE_OVERWRITE);
+            throw new ProjectPropertiesException("CSV file overwrite is not defined in properties file");
+        } else if (csvFileOverwriteStr.toUpperCase().equals("TRUE")) {
+            csvFileOverwriteFlag = true;
+        } else if (csvFileOverwriteStr.toUpperCase().equals("FALSE")) {
+            csvFileOverwriteFlag = false;
+        } else {
+            throw new ProjectPropertiesException("Set CSV File Overwrite to true or false");
+        }
+
+        try {
+            File filePathOrig = new File(csvFilePath);
+            if (!csvFileOverwriteFlag && filePathOrig.isFile()) {
+                System.err.printf("csv.file_path = [%s]\n", KEY_CSV_FILE_PATH);
+                throw new ProjectPropertiesException("File already exists\n" +
+                        "Change file name or enable file overwriting in properties file");
+            }
+        } catch (SecurityException e) {
+            throw new ProjectPropertiesException(e.getLocalizedMessage());
+        }
+
+        try {
+            String fileExtension = FilenameUtils.getExtension(csvFilePath);
+            if (!fileExtension.equals("csv")) {
+                System.err.printf("csv.file_path = [%s]\n", KEY_CSV_FILE_PATH);
+                throw new ProjectPropertiesException("File extension is not .csv\n" +
+                        "Fix CSV file path in properties file");
+            }
+        } catch (SecurityException e) {
+            throw new ProjectPropertiesException(e.getLocalizedMessage());
+        }
+
+        try {
+            File fileFullPath = new File(FilenameUtils.getFullPath(csvFilePath));
+            if (!fileFullPath.isDirectory()) {
+                System.err.printf("csv.file_path = [%s]\n", KEY_CSV_FILE_PATH);
+                throw new ProjectPropertiesException("File directory does not exist\n" +
+                        "Fix CSV file path in properties file");
+            }
+        } catch (SecurityException e) {
+            throw new ProjectPropertiesException(e.getLocalizedMessage());
+        }
+
+        if (debugFlag) {
+            System.out.println("Main.main(): apiKey = [" + apiKey + "]");
+            System.out.println("Main.main(): appId = [" + appId + "]");
+            System.out.println("Main.main(): csvDelimiter = [" + csvDelimiterStr + "]");
+            System.out.println("Main.main(): csvFilePath = [" + csvFilePath + "]");
+            System.out.println("Main.main(): csvFileOverwriteFlag = [" + csvFileOverwriteFlag + "]");
+            System.out.println("Main.main(): csvFileErrorFlag = [" + csvFileErrorFlag + "]");
+            System.out.println("Main.main(): debugFlag = [" + debugFlag + "]");
         }
 
         this.apiKey = apiKey;
         this.appId = appId;
         this.csvDelimiter = csvDelimiter;
-        this.csvFilePath = csvFilePathStr;
+        this.csvFilePath = csvFilePath;
         this.csvFileOverwriteFlag = csvFileOverwriteFlag;
         this.csvFileErrorFlag = csvFileErrorFlag;
         this.debugFlag = debugFlag;
